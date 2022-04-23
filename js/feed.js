fffe53fd857fe5304946d709c6d5aae2e06cd844
plexion <46572320+plexiondev@users.noreply.github.com>
2022-04-23 23:01:04 +0100

    implement basic feed

--- js/feed.js
+++ js/feed.js
@@ -4,16 +4,33 @@
 
 // define xhr GET
 const xhr = new XMLHttpRequest();
-const url = `https://api.mangadex.org/user/follows/manga/feed`;
+const url = `https://api.mangadex.org/user/follows/manga/feed?limit=10`;
 xhr.open('GET', url, true);
+xhr.setRequestHeader('Authorization', `${localStorage.getItem('token')}`);
 
 // on request
 xhr.onload = function() {
     const data = JSON.parse(this.response);
     console.log(data);
 
-    for (let i in data) {
+    for (let i in data.data) {
+        // create element
+        let card = document.createElement('a');
+        card.classList.add('manga-card');
+        card.style.margin = '0';
 
+        // link
+        card.href = `view.html?m=${data.data[i].id}`;
+
+        // text
+        card.innerHTML = (`
+        <div class="info">
+        <h4 class="text-20">${data.data[i].attributes.title}</h4>
+        </div>
+        `);
+
+        // append
+        document.getElementById('feed').appendChild(card);
     }
 }
 
