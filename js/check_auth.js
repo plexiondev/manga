c602021d7bcbdb2a8ec150cd55677a00b2290085
plexion <46572320+plexiondev@users.noreply.github.com>
2022-04-24 13:23:10 +0100

    remove old, moved code

--- js/check_auth.js
+++ js/check_auth.js
@@ -75,11 +75,6 @@ function get_info() {
         let user_id = data.data.id;
         let username = data.data.attributes.username;
 
-        // pass into settings
-        document.getElementById('login_info_temp').innerHTML = `Logged in as <strong>${username}</strong><br><br><a href="/auth.html">Re-authorise</a>`;
-        // remove temp text
-        //document.getElementById('login_info_temp').remove;
-
         // cache info
         localStorage.setItem('token_user_info',`${this.response}`);
         localStorage.setItem('token_user_id',`${user_id}`);
