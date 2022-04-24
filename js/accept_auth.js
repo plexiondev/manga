64bed3eaf01386de42776388b8f85c1863faf1a9
plexion <46572320+plexiondev@users.noreply.github.com>
2022-04-24 18:00:11 +0100

    fix logging (and display) in accept_auth.js

--- js/accept_auth.js
+++ js/accept_auth.js
@@ -8,5 +8,5 @@ const acceptauth_url = new URLSearchParams(acceptauth_query);
 // check if just logged in
 const logged_in = acceptauth_url.get('logged_in') || undefined;
 if (logged_in == 1) {
-    log('general',`You're logged in!`);
+    log('auth',`You're logged in!`,false);
 }
