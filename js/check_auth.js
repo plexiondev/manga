062b0dd4f027d80dbd50a07e0a8ce0c7dae7e613
plexion <46572320+plexiondev@users.noreply.github.com>
2022-04-24 13:17:21 +0100

    integrate show_login.js

--- js/check_auth.js
+++ js/check_auth.js
@@ -57,3 +57,39 @@ function refresh_auth(redirect) {
     log('general',`Authorised again until ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} (14 min)`);
     localStorage.setItem('token_cache', now);
 }
+
+
+// get user info
+function get_info() {
+    // define XHR GET
+    const xhr = new XMLHttpRequest();
+    const url = `https://api.mangadex.org/user/me`;
+    xhr.open('GET', url, true);
+    xhr.setRequestHeader('Authorization', `${localStorage.getItem('token')}`);
+
+    xhr.onload = function() {
+        const data = JSON.parse(this.response);
+
+        let user_id = data.data.id;
+        let username = data.data.attributes.username;
+
+        // pass into settings
+        document.getElementById('login_info_temp').innerHTML = `Logged in as <strong>${username}</strong><br><br><a href="/auth.html">Re-authorise</a>`;
+        // remove temp text
+        //document.getElementById('login_info_temp').remove;
+
+        // cache info
+        localStorage.setItem('token_user_info',`${this.response}`);
+        localStorage.setItem('token_user_id',`${user_id}`);
+        localStorage.setItem('token_username',`${username}`);
+    }
+
+    // send off
+    xhr.send();    
+}
+
+
+// embed info
+function embed_info(element) {
+    element.innerHTML = `Logged in as <strong>${localStorage.getItem('token_username')}</strong><br><br><a href="/auth.html">Re-authorise</a>`;
+}
