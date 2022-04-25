fa17fa1529d5da6ab0125fa83de3b5d383172384
plexion <46572320+plexiondev@users.noreply.github.com>
2022-04-25 21:00:13 +0100

    add windows.js

---
+++ js/windows.js
@@ -0,0 +1,14 @@
+
+// show pop-up windows
+
+
+let em_window_parent = document.getElementById('window_parent');
+
+// check if parent has children
+setTimeout(function() {
+    if (em_window_parent.hasChildNodes()) {
+        em_window_parent.classList.add('shown');
+    } else {
+        em_window_parent.classList.remove('shown');
+    }
+}, 100);
