bca444ef16c1ce4bb952c5d010da49d1b06bd526
plexion <46572320+plexiondev@users.noreply.github.com>
2022-04-30 22:19:26 +0100

    cut off description on feed

--- js/feed.js
+++ js/feed.js
@@ -95,7 +95,7 @@ function create_em(data_pass,cover_url_pass,manga_pass,i) {
     em_info.classList.add('info');
     em_info.innerHTML = (`
     <h4 class="text-20">${data.data[i].attributes.title.en}</h4>
-    <p class="text-16">${data.data[i].attributes.description.en}</p>
+    <div class="desc-cont"><p class="text-16">${data.data[i].attributes.description.en}</p></div>
     <label class="tag ${data.data[i].attributes.contentRating}" style="margin-left: 0;"><i class="icon w-16" data-feather="${tags_icon[`${data.data[i].attributes.contentRating}`]}" style="margin-right: 3px; top: -1.3px !important;"></i>${rating}</label>
     `);
 
