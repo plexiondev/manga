659c9e94a6878cc34183911e77003f8e3691ed16
plexion <46572320+plexiondev@users.noreply.github.com>
2022-04-30 14:50:49 +0100

    change behaviour of mark read

--- js/mark_read.js
+++ js/mark_read.js
@@ -75,7 +75,8 @@ function send_read() {
     console.log(`Read: ${read}\n\nUnread: ${unread}`);
 
     // send
-    if (read.length > 0 && unread.length > 0) {
+    if (read.length != 0 && unread.length != 0) {
+        // updates for read & unread
         sr_xhr.send(JSON.stringify({
             "chapterIdsRead": [
                 `${read}`
@@ -84,13 +85,15 @@ function send_read() {
                 `${unread}`
             ]
         }));
-    } else if (read.length > 0 && unread.length >! 0) {
+    } else if (read.length != 0 && unread.length == 0) {
+        // updates for read only
         sr_xhr.send(JSON.stringify({
             "chapterIdsRead": [
                 `${read}`
             ]
         }));        
-    } else if (read.length >! 0 && unread.length > 0) {
+    } else if (read.length == 0 && unread.length != 0) {
+        // updates for unread only
         sr_xhr.send(JSON.stringify({
             "chapterIdsUnread": [
                 `${unread}`
