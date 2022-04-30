cdf7b176a17b7b1546e5812b8b1d63d9aa65cffa
plexion <46572320+plexiondev@users.noreply.github.com>
2022-04-30 22:31:51 +0100

    append `.html` to header links

--- js/header.js
+++ js/header.js
@@ -6,11 +6,11 @@ document.getElementById('header').innerHTML =
 <span class="links">
     <ul>
     <li><a href="/"><i class="icon w-20" data-feather="home"></i>Home</a></li>
-    <li><a href="/search"><i class="icon w-20" data-feather="search"></i>Search</a></li>
+    <li><a href="/search.html"><i class="icon w-20" data-feather="search"></i>Search</a></li>
     </ul>
     <hr>
     <ul>
-    <li><a href="/docs"><i class="icon w-20" data-feather="book"></i>Docs</a></li>
+    <li><a href="/docs.html"><i class="icon w-20" data-feather="book"></i>Docs</a></li>
     <li><a href="https://github.com/plexiondev/manga"><i class="icon w-20" data-feather="external-link"></i>View Source</a></li>
     <li><a href="/settings"><i class="icon w-20" data-feather="settings"></i>Settings</a></li>
     </ul>
