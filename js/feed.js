e0ac21bc32b844cd7e92058909aaf0be782761d8
plexion <46572320+plexiondev@users.noreply.github.com>
2022-05-01 13:50:28 +0100

    add pages to feed

--- js/feed.js
+++ js/feed.js
@@ -1,6 +1,11 @@
 // home feed
 // requires auth via auth.js
 
+var limit = 70;
+var offset = 0;
+var top_limit = 140;
+
+load_page();
 
 const contentrating_string = {
     'safe': 'Safe',
@@ -17,27 +22,50 @@ const tags_icon = {
     'pornographic': 'alert-octagon'
 }
 
-// define xhr GET
-const xhr = new XMLHttpRequest();
-const url = `https://api.mangadex.org/user/follows/manga/?includes[]=cover_art&limit=70`;
-xhr.open('GET', url, true);
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.setRequestHeader('Authorization', `${localStorage.getItem('token')}`);
+function load_page() {
+    // define xhr GET
+    const xhr = new XMLHttpRequest();
+    const url = `https://api.mangadex.org/user/follows/manga/?includes[]=cover_art&limit=${limit}&offset=${offset}`;
+    xhr.open('GET', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Authorization', `${localStorage.getItem('token')}`);
 
-// on request
-xhr.onload = function() {
-    const data = JSON.parse(this.response);
-    
-    for (let i in data.data) {
-        // get manga id
-        var manga = data.data[i].id;
+    // on request
+    xhr.onload = function() {
+        const data = JSON.parse(this.response);
+        document.getElementById('feed').innerHTML = ``;
+
+        // reset total
+        if (top_limit > data.total) {
+            top_limit = data.total;
+        }
+
+        // calculate total
+        var total = Math.round(data.total / limit); // 21418 = 1189
+        var page = Math.round(offset / limit); // 18 = 1
+        var top_limit_round = Math.round(top_limit / limit); // 3600 = 200
+
+        // pages
+        document.getElementById('advance_pages').innerHTML =
+        (`
+        <button class="page-num left" onclick="set_page(0)">1</button>
+        <button class="page-num" onclick="set_page(${offset-limit})">${page-1}</button>
+        <button class="page-num current" onclick="set_page(${offset})">${page}</button>
+        <button class="page-num" onclick="set_page(${offset+limit})">${page+1}</button>
+        <button class="page-num right" onclick="set_page(${top_limit})">${top_limit_round}</button>
+        `);
+        
+        for (let i in data.data) {
+            // get manga id
+            var manga = data.data[i].id;
             
-        get_relationships(this.response,manga,i);
+            get_relationships(this.response,manga,i);
+        }
     }
-}
 
-// send
-xhr.send();
+    // send
+    xhr.send();
+}
 
 function get_relationships(data_pass,manga_pass,i) {
 
@@ -126,4 +154,37 @@ function create_em(data_pass,cover_url_pass,manga_pass,i) {
     document.getElementById('feed').appendChild(card);
 
     feather.replace();
+}
+
+// advance pages
+function advance_page(direction) {
+    if (direction == 1) {
+        offset += limit;
+        if (offset <= 3600) {
+            log('general',`Advancing forward 1 page (${offset})`,true);
+            load_page();
+        } else {
+            offset -= limit;
+        }
+    } else {
+        offset -= limit;
+        if (offset >= 0) {
+            log('general',`Advancing backward 1 page (${offset})`,true);
+            load_page();
+        } else {
+            offset += limit;
+        }
+    }
+}
+
+// set page directly
+function set_page(page) {
+    offset_temp = offset;
+    offset = page;
+    if (offset >= 0 && offset <= 3600) {
+        log('general',`Set page to ${offset}`,true);
+        load_page();
+    } else {
+        offset = offset_temp;
+    }
 }
