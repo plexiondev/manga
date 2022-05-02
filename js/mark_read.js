495c2c5fbcc5732135e74ff741182b91c30db961
plexion <46572320+plexiondev@users.noreply.github.com>
2022-05-02 21:57:02 +0100

    clean up (new method based on frontend MD)

--- js/mark_read.js
+++ js/mark_read.js
@@ -7,29 +7,26 @@ function mark_read(chapter_id_pass,force) {
     var chapter_id = chapter_id_pass;
 
     if (localStorage.getItem(`${chapter_id}_read`) == 0 || localStorage.getItem(`${chapter_id}_read`) == null || force == true) {
+        // mark as read
         if (force == true) {
+            // onload, not user requested
             document.getElementById(`mark_${chapter_id}`).classList.add('read');
         } else {
+            // user requested (by clicking)
             send_read(chapter_id);
         }
     } else {
         // mark as unread
-        localStorage.removeItem(`${chapter_id}_read`);
-        document.getElementById(`mark_${chapter_id}`).classList.remove('read');
-
         remove_read(chapter_id);
-
-        // log to user
-        log('enabled',`Marked ${chapter_id} as unread.`,false);
     }
 }
 
 
 // mark as read
 function send_read(chapter_id) {
-    // define xhr GET
+    // define xhr POST
     const xhr = new XMLHttpRequest();
-    const url = `https://api.mangadex.org/manga/${manga}/read`;
+    const url = `https://api.mangadex.org/chapter/${chapter_id}/read`;
     xhr.open('POST',url,true);
     xhr.setRequestHeader('Content-Type','application/json');
     xhr.setRequestHeader('Authorization',`${localStorage.getItem('token')}`);
@@ -40,5 +37,23 @@ function send_read(chapter_id) {
         log('enabled',`Marked ${chapter_id} as read.`,false);
     }
 
-    xhr.send(JSON.stringify(unread));
+    xhr.send();
+}
+
+// mark as unread
+function remove_read(chapter_id) {
+    // define xhr DELETE
+    const xhr = new XMLHttpRequest();
+    const url = `https://api.mangadex.org/chapter/${chapter_id}/read`;
+    xhr.open('DELETE',url,true);
+    xhr.setRequestHeader('Content-Type','application/json');
+    xhr.setRequestHeader('Authorization',`${localStorage.getItem('token')}`);
+
+    xhr.onload = function() {
+        // log to user
+        document.getElementById(`mark_${chapter_id}`).classList.remove('read');
+        log('disabled',`Marked ${chapter_id} as unread.`,false);
+    }
+
+    xhr.send();
 }
