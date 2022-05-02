d881c6ea7c70964dbf8d122e104b203978ba8882
plexion <46572320+plexiondev@users.noreply.github.com>
2022-05-02 13:12:46 +0100

    add profile to navbar

--- js/header.js
+++ js/header.js
@@ -12,14 +12,12 @@ document.getElementById('header').innerHTML =
     <ul>
     <li><a href="/"><i class="icon w-20" data-feather="home"></i>Home</a></li>
     <li><a href="/search.html"><i class="icon w-20" data-feather="search"></i>Search</a></li>
+    <li><a href="/tags.html"><i class="icon w-20" data-feather="tag"></i>Tags</a></li>
     <li><a href="/recent.html"><i class="icon w-20" data-feather="plus-circle"></i>Recent</a></li>
     </ul>
     <hr>
     <ul>
-    <li><a href="/tags.html"><i class="icon w-20" data-feather="tag"></i>Tags</a></li>
-    </ul>
-    <hr>
-    <ul>
+    <li><a href="/user.html?u=${localStorage.getItem('token_user_id')}"><i class="icon w-20" data-feather="user"></i>Profile</a></li>
     <li><a href="/groups.html"><i class="icon w-20" data-feather="users"></i>Groups</a></li>
     </ul>
     <hr>
