c11d8c17e0c9374596821920437c02d882c22efb
plexion <46572320+plexiondev@users.noreply.github.com>
2022-06-28 18:41:07 +0100

    handle `undefined`

--- js/titles.js
+++ js/titles.js
@@ -4,19 +4,27 @@
 const title_priority = ['en','ja','ko','other'];
 
 function parse_title(data) {
+    let text = '';
+
     for (let priority in title_priority) {
         for (let i in data) {
-            if (i == title_priority[priority]) return data[i];
+            if (i == title_priority[priority]) text = data[i];
         }
     }
+
+    return text;
 }
 
 function parse_alt_title(data) {
+    let text = '';
+
     for (let priority in title_priority) {
         for (let n in data) {
             for (let i in data[n]) {
-                if (i == title_priority[priority]) return data[n][i];
+                if (i == title_priority[priority]) text = data[n][i];
             }
         }
     }
+
+    return text;
 }
